43d996decc45c96b853793082f8be4a1a26faddb
matheusvskt <matheusvieira99@gmail.com>
2022-01-05 17:11:01 -0300

    first server

---
+++ Primeiro Servidor de Páginas com Node/index.js
@@ -0,0 +1,30 @@
+//primeiro servidor web com node js
+
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+
+http.createServer((request, response) =>{
+
+    let path = (url.parse(request.url).pathname);
+    let fileName = "." + path;
+
+    fs.readFile(fileName,(error, data)=>{
+        if(error){
+            response.writeHead(484,{"Content-Type":"text/html;charset=utf-8"});
+            response.end("Página não encontrada")
+        }else{
+            response.writeHead(200,{"Content-Type":"text/html"})
+            response.writeHead(data); 
+            response.end();
+
+        }
+    })
+   
+}).listen(3000, (error)=>{
+    if(error){
+        console.log(error)
+    }else{
+        console.log("Servidor rodando")
+    }
+})
